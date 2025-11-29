49c3bd9f4df700155fd2d3414212dcfb154f3d5a
Prakash Poudel <mee.prakash25@gmail.com>
2025-11-29 15:23:20 +0545

    make project deployment ready on render

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,6 +4,7 @@ import "dotenv/config"
 import mongoose from "mongoose"
 import { errorHandler } from "./middleware/errorHandler"
 import UserRoute from "./routes/UserRoute"
+import path from "path"
 
 mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING as string)
@@ -11,11 +12,21 @@ mongoose
 
 const app = express()
 app.use(express.json())
-app.use(cors())
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  })
+)
 
 app.use("/api/user", UserRoute)
 
 app.use(errorHandler)
+
+app.use(express.static(path.join(__dirname, "../../frontend/dist")))
+
+app.use((req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"))
+})
 
 const PORT = process.env.PORT || 8000
 app.listen(PORT, () => {
