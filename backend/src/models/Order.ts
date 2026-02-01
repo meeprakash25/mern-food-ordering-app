ad0fea7f455ee165fef2dae621e4122aeb0c3de7
Prakash Poudel <mee.prakash25@gmail.com>
2026-02-01 21:10:35 +0545

    stripe refund event

--- backend/src/models/Order.ts
+++ backend/src/models/Order.ts
@@ -37,7 +37,7 @@ const orderSchema = new mongoose.Schema(
     },
     paymentStatus: {
       type: String,
-      enum: ["paid", "unpaid", "stripeError", "rejected","cancelled"],
+      enum: ["paid", "unpaid", "stripeError", "rejected", "cancelled", "refunded"],
       default: "unpaid",
     },
   },
